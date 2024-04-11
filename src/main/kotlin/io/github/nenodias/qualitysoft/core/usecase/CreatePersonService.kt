2d4c0c87d50ad00799c7a7a117cc796028a4964f
nenodias <horacio.dias92@gmail.com>
2024-04-11 09:08:16 -0300

    Adjusting @Primary because there is more than one implementation of interface

--- src/main/kotlin/io/github/nenodias/qualitysoft/core/usecase/CreatePersonService.kt
+++ src/main/kotlin/io/github/nenodias/qualitysoft/core/usecase/CreatePersonService.kt
@@ -3,8 +3,10 @@ package io.github.nenodias.qualitysoft.core.usecase
 import io.github.nenodias.qualitysoft.core.domain.Person
 import io.github.nenodias.qualitysoft.core.ports.input.CreatePersonPortIn
 import io.github.nenodias.qualitysoft.core.ports.output.PersistPersonPortOut
+import org.springframework.context.annotation.Primary
 import org.springframework.stereotype.Service
 
+@Primary
 @Service
 class CreatePersonService(
         private val persistPersonPortOut: PersistPersonPortOut
